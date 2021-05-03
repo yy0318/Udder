b03280b1a07fe2f130e0a07a192c10e8233d3db4
Ang Yu Yang <yy_0318@live.com>
2021-05-03 15:53:58 +0900

    Add Label

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,8 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("Udder")
+            Text("Fresh Mlik, On Demand")
+                .padding()
+        }
     }
 }
 
